7265b5f7701008904aacd98c22db737b4caac425
yiwei <e-well@outlook.com>
2024-11-14 19:47:10 -0500

    more checkpoints and learning rates

--- val_grad.sh
+++ val_grad.sh
@@ -1,10 +1,16 @@
 
-CKPT=92
 MODEL_LOG="TinyLlama/TinyLlama-1.1B-Chat-v1.0-p0.1-lora-seed3"
-MODEL_PATH=../out/${MODEL_LOG}/checkpoint-${CKPT}
+
 TASK=mmlu
-OUTPUT_PATH=../grads/${MODEL_LOG}/${TASK}-ckpt${CKPT}-sgd # for validation data, we always use sgd
+
 DATA_DIR=data
 DIMS="8192" # We use 8192 as our default projection dimension 
 
-./less/scripts/get_info/grad/get_eval_lora_grads.sh "$TASK" "$DATA_DIR" "$MODEL_PATH" $OUTPUT_PATH "$DIMS"
+
+for CKPT in 23 46 70 92; do
+    echo "Processing checkpoint ${CKPT}"
+
+    MODEL_PATH=../out/${MODEL_LOG}/checkpoint-${CKPT}
+    OUTPUT_PATH=../grads/${MODEL_LOG}/${TASK}-ckpt${CKPT}-sgd # for validation data, we always use sgd
+    ./less/scripts/get_info/grad/get_eval_lora_grads.sh "$TASK" "$DATA_DIR" "$MODEL_PATH" $OUTPUT_PATH "$DIMS"
+done
