53bcc4124046505a409ca234fc2ba57bcbf4e505
yiwei <e-well@outlook.com>
2024-11-20 23:29:23 -0500

    update file path

--- evaluation/raw_eval.sh
+++ evaluation/raw_eval.sh
@@ -1,9 +1,10 @@
-
-CUDA_VISIBLE_DEVICES=1 python -m eval.mmlu.run_eval \
+GPU_ID=${1:-all}
+export CUDA_VISIBLE_DEVICES=$GPU_ID
+python -m eval.mmlu.run_eval \
     --ntrain 5 \
-    --data_dir ../data/eval/mmlu \
-    --save_dir result \
-    --model_name_or_path ../../out/TinyLlama/TinyLlama-1.1B-Chat-v1.0-p0.1-lora \
-    --tokenizer_name_or_path ../../out/TinyLlama/TinyLlama-1.1B-Chat-v1.0-p0.1-lora \
+    --data_dir ~/data/llm/eval/mmlu \
+    --save_dir result/filtered/clf \
+    --model_name_or_path ~/out/TinyLlama/TinyLlama-1.1B-Chat-v1.0-p0.5-lora-clf-filtered \
+    --tokenizer_name_or_path ~/out/TinyLlama/TinyLlama-1.1B-Chat-v1.0-p0.5-lora-clf-filtered \
     --eval_batch_size 4 \
     --convert_to_bf16
